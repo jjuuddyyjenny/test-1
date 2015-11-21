bbaca5fdadeb5af0cb6ec95a694bcb48befa9217
jjuuddyyjenny <jjuuddyyjenny@gmail.com>
2015-11-21 17:55:31 +0800

    added a java

---
+++ haha.java
@@ -0,0 +1,11 @@
+//write  program to print out "haha"
+public class haha
+{
+    public static void main (String[] args)
+    {
+        System.out.print("I'm always hungry");
+       
+      
+    }
+}    
+ 
